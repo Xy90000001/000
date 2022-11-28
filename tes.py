f33fcc925db155ce6e164cb732d9087ab1e96f14
Xy90000001 <106469364+Xy90000001@users.noreply.github.com>
2022-11-28 10:08:02 -0800

    Update tes.py

--- tes.py
+++ tes.py
@@ -5,13 +5,32 @@ from flask import Flask, render_template, request
 # rep = 0
 # global rep
 app = Flask(__name__)
-
+l=[]
+def loop():
+  a = 0
+  while l<=15:
+    a+=1
+    time.sleep(2)
+    l.append(a)
 
 # @app.route('/')#, methods=['GET','POST'])
 # def my_form_post():
 @app.route('/')
 def my_form_post():
-  return 'duh!....!'
+  return 'duh!....!'+str(a)+str(l)
 
+def web():    
+  app.run(debug=True)
+
+
+    
 # if __name__==__main__:
-app.run(debug=True)
+import threading
+if __name__ == "__main__":
+    x = threading.Thread(target=loop)#, args=(a,))
+   # threads.append(x)
+    x.start()
+    time.sleep(1)
+        
+web()
+
