13430de3060eb75a09f099694830b4c8ba7cec72
Xy90000001 <106469364+Xy90000001@users.noreply.github.com>
2022-12-23 10:04:15 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,16 @@ from flask import Flask, render_template, request
 import subprocess as s
 import time, os
 
-
 app = Flask(__name__)
 
-file1 = open('cach.txt', 'r')
-# print(file.read())
-f = file1.read()
+# file1 = open('cach.txt', 'r')
+# # print(file.read())
+# f = file1.read()
 
 
 @app.route('/')
 def appp():
-    return 'duh  '+str(f)
+    return 'duh  '
 
 if __name__=="__main__":
     app.run(debug=True)
