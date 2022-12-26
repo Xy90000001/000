96b57491af1d6673dec9032b85d252972d806576
Xy90000001 <106469364+Xy90000001@users.noreply.github.com>
2022-12-26 09:51:01 -0800

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,13 +1,14 @@
 import time
-
+file =  open('cach.txt','w')
+file.write('file')
 a = 0
 l=[]
 while a<=11:
   a+=1
   l.clear()
   l.append(a)
-  file =  open('cach.txt','w')
-  file.write(str(l))
+  file1 =  open('cach.txt','w')
+  file1.write(str(l))
   time.sleep(2)
 
   
